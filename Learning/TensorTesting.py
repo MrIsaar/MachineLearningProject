0ec165d0039d3beb5779a48cc5140580360604a0
MrIsaar <47126870+MrIsaar@users.noreply.github.com>
2021-12-15 15:21:16 -0700

    testtorchvision

--- Learning/TensorTesting.py
+++ Learning/TensorTesting.py
@@ -2,14 +2,17 @@ import math
 import numpy as np
 import torch
 
-data = [[1,2],[3,4]]
+data = [[1,2,3,4]]
 nparr = np.array(data)
 x_np = torch.from_numpy(nparr)
 ones = torch.ones((2,3))
-print(f"Tensor: \n {ones}\n")
-tensor = torch.rand((2,3))
+tensor = torch.ones(4, 4)
+print(tensor)
+tensor[:,1] = 0
+print(tensor.add_(x_np))
 # We move our tensor to the GPU if available
 if torch.cuda.is_available():
   tensor = tensor.to('cuda')
   print(f"Device tensor is stored on: {tensor.device}")
 print(tensor.device)
+print(x_np.numpy())
