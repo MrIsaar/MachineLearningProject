bd6cf125359e777e039f743ecd1474e117dded54
MrIsaar <47126870+MrIsaar@users.noreply.github.com>
2021-12-15 14:52:00 -0700

    test gpu avail

--- Learning/recommender.py
+++ Learning/recommender.py
@@ -3,8 +3,14 @@ import numpy as np
 import scipy
 import torch
 
-def useTorch():
-    torch.nn.init()
-    
-print("Running Recommender")
-print("done")
+data = [[1,2],[3,4]]
+nparr = np.array(data)
+x_np = torch.from_numpy(nparr)
+ones = torch.ones((2,3))
+print(f"Tensor: \n {ones}\n")
+tensor = torch.rand((2,3))
+# We move our tensor to the GPU if available
+if torch.cuda.is_available():
+  tensor = tensor.to('cuda')
+  print(f"Device tensor is stored on: {tensor.device}")
+print(tensor.device)
